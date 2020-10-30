95ad8beffc18219eedb59f63f1cd3e0f00a08d2c
masonhorder <masonhorder@gmail.com>
2020-10-30 15:24:00 -0700

    final

--- final.py
+++ final.py
@@ -1,22 +1,11 @@
 returnList = []
 def repeatCheck(a, returnList):
-    # print(returnList)
     for b in a:
         if type(b) == list:
             repeatCheck(b,returnList)
         else:
             returnList.append(b)
     return returnList
-
-# def subListConvert(a):
-
-
-# if type(a) == list:
-#     repeatCheck(a,returnList)
-# else:
-#     returnList.append(a)            
-
-
-
+      
 
 print(repeatCheck([34, 3, 5, 9, 12, 53, 9, [4, 3, 1, 56, 8, [1, 3, 5], [3, 9, 9, [5, 3]], 9, 3], [3, 5, [3, 3, 2], [3, 2, 5]]], returnList))
